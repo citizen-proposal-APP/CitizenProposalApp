c0bbbde257d6ccb8b0b59d40d2fe9f2df68fd6f9
palapapa <palapapa93@gmail.com>
2024-10-13 23:27:44 +0800

    Added HSTS and developer exception page

--- backend/Sources/Program.cs
+++ backend/Sources/Program.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
 
 namespace CitizenProposalApp;
 
@@ -7,7 +9,17 @@ internal class Program
     private static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddHsts(options =>
+        {
+            options.MaxAge = TimeSpan.FromDays(365);
+            options.IncludeSubDomains = true;
+        });
         WebApplication app = builder.Build();
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage()
+                .UseHsts();
+        }
         app.MapGet("/", () => "Hello World!");
         app.Run();
     }
