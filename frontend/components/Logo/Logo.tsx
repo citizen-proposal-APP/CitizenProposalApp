f73ea715d0480698e60ceabf2e857132b1f7b666
wytsai7660 <wytsai7660@gmail.com>
2024-10-29 22:14:52 +0800

    Improve logo component
    - Replaced placeholder text with the actual app name
    - Enhanced text layout for better readability

--- frontend/components/Logo/Logo.tsx
+++ frontend/components/Logo/Logo.tsx
@@ -1,13 +1,21 @@
-import Image from 'next/image';
-import { Group, Text } from '@mantine/core';
+import Link from 'next/link';
+import { Group, Image, Stack, Text } from '@mantine/core';
+import classes from './Logo.module.css';
 
 export function Logo({ size }: { size: number }) {
   return (
-    <Group>
-      <Image src="/logo.png" alt="Logo" width={size} height={size} />
-      <Text size="xl" fw={700} ml={-10} c="SeaGreen">
-        網站名佔位符
-      </Text>
-    </Group>
+    <Link href="/" style={{ textDecoration: 'none' }}>
+      <Group>
+        <Image src="/logo.png" alt="Logo" h={size} w="auto" />
+        <Stack gap={0} justify="center">
+          <Text className={classes.disperse} size="lg" fw={700} c="SeaGreen" mb={-6}>
+            國民之聲
+          </Text>
+          <Text className={classes.disperse} size="sm" fw={500} c="SeaGreen" mb={3}>
+            Nation Voice
+          </Text>
+        </Stack>
+      </Group>
+    </Link>
   );
 }
