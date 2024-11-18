396c7af3cc550a23cea8202ddfbb7c21dd595139
Maguta2003 <a0966085547@gmail.com>
2024-11-18 15:33:38 +0800

    Basic UI for proposal edit page

---
+++ frontend/pages/edit.tsx
@@ -0,0 +1,11 @@
+import { EditProposal } from '../components/EditProposal/EditProposal';
+import { Layout } from '@/components/Layout/Layout';
+import React from "react";
+
+export default function EditPage() {
+  return (
+    <Layout>
+      <EditProposal />
+    </Layout>
+  );
+}
