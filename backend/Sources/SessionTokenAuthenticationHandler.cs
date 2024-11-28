b7185805028aa74f1435420e15554c778b318097
palapapa <palapapa93@gmail.com>
2024-11-28 13:01:22 +0800

    Added the missing check of whether the session token has expired

--- backend/Sources/SessionTokenAuthenticationHandler.cs
+++ backend/Sources/SessionTokenAuthenticationHandler.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 namespace CitizenProposalApp;
 
 #pragma warning disable CA1812
-internal sealed class SessionTokenAuthenticationHandler(IOptionsMonitor<SessionTokenAuthenticationHandlerOptions> options, ILoggerFactory logger, UrlEncoder encoder, CitizenProposalAppDbContext dbContext) : AuthenticationHandler<SessionTokenAuthenticationHandlerOptions>(options, logger, encoder)
+internal sealed class SessionTokenAuthenticationHandler(IOptionsMonitor<SessionTokenAuthenticationHandlerOptions> options, ILoggerFactory logger, UrlEncoder encoder, CitizenProposalAppDbContext dbContext, TimeProvider timeProvider) : AuthenticationHandler<SessionTokenAuthenticationHandlerOptions>(options, logger, encoder)
 {
     protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
     {
@@ -27,6 +27,10 @@ internal sealed class SessionTokenAuthenticationHandler(IOptionsMonitor<SessionT
         if (session is null)
         {
             return AuthenticateResult.Fail("The \"session\" cookie doesn't contain a valid session token.");
+        }
+        if (session.ExpirationTime < timeProvider.GetUtcNow())
+        {
+            return AuthenticateResult.Fail("The \"session\" cookie has expired.");
         }
         int userId = dbContext.Entry(session).Property<int>("UserId").CurrentValue;
         List<Claim> claims = [
