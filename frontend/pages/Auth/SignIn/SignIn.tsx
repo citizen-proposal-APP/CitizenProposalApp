ba8ce6c9f73526f3e5daf9ffcbb61f30c964d700
wytsai7660 <wytsai7660@gmail.com>
2024-12-07 02:00:50 +0800

    Remove forgot password functionality
    (The `ForgotPassword` component.)

--- frontend/pages/Auth/SignIn/SignIn.tsx
+++ frontend/pages/Auth/SignIn/SignIn.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
 import {
   Anchor,
   Button,
   Container,
-  Group,
   Paper,
   PasswordInput,
   Text,
   TextInput,
   Title,
 } from '@mantine/core';
-import { ForgotPassword } from '../ForgotPassword/ForgotPassword';
 import classes from './SignIn.module.css';
 
 interface AuthenticationTitleProps {
@@ -18,39 +15,24 @@ interface AuthenticationTitleProps {
 }
 
 export function AuthenticationTitle({ onToggle }: AuthenticationTitleProps) {
-  const [showForgotPassword, setShowForgotPassword] = useState(false);
-
   return (
     <Container size={420} my={40}>
-      {showForgotPassword ? (
-        <ForgotPassword onBack={() => setShowForgotPassword(false)} />
-      ) : (
-        <>
-          <Title ta="center" className={classes.title}>
-            歡迎回來！
-          </Title>
-
-          <Text c="dimmed" size="sm" ta="center" mt={5}>
-            還沒有帳號嗎?{' '}
-            <Anchor size="sm" component="button" onClick={onToggle}>
-              建立帳號
-            </Anchor>
-          </Text>
-
-          <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-            <TextInput label="使用者名稱" placeholder="您的使用者名稱" required />
-            <PasswordInput label="密碼" placeholder="您的密碼" required mt="md" />
-            <Group justify="space-between" mt="lg">
-              <Anchor component="button" size="sm" onClick={() => setShowForgotPassword(true)}>
-                忘記密碼？
-              </Anchor>
-            </Group>
-            <Button fullWidth mt="xl">
-              登入
-            </Button>
-          </Paper>
-        </>
-      )}
+      <Title ta="center" className={classes.title}>
+        歡迎回來！
+      </Title>
+      <Text c="dimmed" size="sm" ta="center" mt={5}>
+        還沒有帳號嗎?{' '}
+        <Anchor size="sm" component="button" onClick={onToggle}>
+          建立帳號
+        </Anchor>
+      </Text>
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="使用者名稱" placeholder="您的使用者名稱" required />
+        <PasswordInput label="密碼" placeholder="您的密碼" required mt="md" />
+        <Button fullWidth mt="xl">
+          登入
+        </Button>
+      </Paper>
     </Container>
   );
 }
