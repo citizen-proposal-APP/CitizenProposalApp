6cc8b9721a87ed6d3cef69f55d303340ad8deb86
wytsai7660 <wytsai7660@gmail.com>
2024-12-09 15:37:21 +0800

    Update `_app.tsx`

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
 
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
@@ -9,12 +10,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
       <Head>
-        <title>Mantine Template</title>
+        <title>國民之聲 Nation Voice</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/logo.png" />
       </Head>
       <Component {...pageProps} />
     </MantineProvider>
