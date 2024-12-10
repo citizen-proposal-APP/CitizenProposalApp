94254256d2df304f50aa31c07016818469bf804f
AlanLiang-314 <alanliang314159@gmail.com>
2024-12-10 19:18:17 +0800

    Add download script for pretrained models

---
+++ AI/download_models.sh
@@ -0,0 +1,8 @@
+wget https://huggingface.co/alan314159/CitizenProposalApp/resolve/main/department_label.zip
+wget https://huggingface.co/alan314159/CitizenProposalApp/resolve/main/onnx.zip
+wget https://huggingface.co/alan314159/CitizenProposalApp/resolve/main/proposal.index
+wget https://huggingface.co/alan314159/CitizenProposalApp/resolve/main/proposal_cluster_labels.pkl
+unzip department_label.zip
+unzip onnx.zip
+rm department_label.zip
+rm onnx.zip
