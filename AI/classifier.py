c2ef72c7d80e5bf88644ab427435e29735b1b113
AlanLiang-314 <alanliang314159@gmail.com>
2024-12-12 07:45:25 +0000

    Better code structure

--- AI/classifier.py
+++ AI/classifier.py
@@ -34,59 +34,60 @@ class EmsembleSVM():
         tag_id = np.argmax(result, axis=1)[0]
         tag = self.id_2_label[tag_id]
         return tag, tag_id
-
-model = SentenceTransformer("model/onnx", backend="onnx")
-classifier = EmsembleSVM('model/department_label')
-
-topic_label_index = Index(ndim=1792)
-topic_label_index.load("model/topic_label/proposal.index")
-
-index = Index(ndim=1792)
-os.makedirs("db", exist_ok=True)
-if not os.path.exists("db/main.index"):
-    index.save("db/main.index")
-index.load("db/main.index")
-
-with open("model/topic_label/proposal_cluster_labels.pkl", "rb") as f:
-    x, cluster_labels, id_2_label = pickle.load(f)
-
-def embed(id: int, x: str):
-    vector = model.encode(x)
-    index.add(id, vector)
-
-async def save_db():
-    index.save("db/main.index")
-
-def index_rank(query: str, topk: int):
-    query_embedding = model.encode(query)
+    
+class Ranker():
+    def __init__(self, model_path: str, db_path: str):
+        self.index = Index(ndim=1792)
+        self.topic_label_index = Index(ndim=1792)
         
-    result = index.search(query_embedding, topk)
-    return result.keys
-
-def get_tags(query: str):
-    query_embedding = model.encode(query)
-    
-    if query_embedding.ndim == 1:
-        query_embedding = query_embedding[None, :]
-    result = classifier.predict(query_embedding)
-    department_label, department_label_id = classifier.get_label(result)
-    
-    result = topic_label_index.search(query_embedding, 5)
-    print("\n".join(x[k] for k in result.keys))
-    print(cluster_labels[result.keys])
-    most_similar = Counter(cluster_labels[result.keys]).most_common(1)[0][0]
-    print(f"Most similar proposal: {most_similar}")
-    print([id_2_label[k] for k in cluster_labels[result.keys]])
-    
-    if id_2_label[most_similar] != None:
-        topic_label = id_2_label[most_similar].strip("\n")
-    else:
-        topic_label = None
+        os.makedirs(db_path, exist_ok=True)
+        if not os.path.exists(f"{db_path}/main.index"):
+            self.index.save(f"{db_path}/main.index")
+        self.index.load(f"{db_path}/main.index")
         
-    return department_label, department_label_id, topic_label, most_similar + (classifier.num_labels + 1)
+        self.model = SentenceTransformer(f"{model_path}/onnx", backend="onnx")
+        
+        self.classifier = EmsembleSVM(f'{model_path}/department_label')
+        
+        self.topic_label_index.load(f"{model_path}/topic_label/proposal.index")
+        
+        with open("model/topic_label/proposal_cluster_labels.pkl", "rb") as f:
+            x, self.cluster_labels, self.id_2_label = pickle.load(f)
+        
+        del x
+ 
+    def embed(self, id: int, x: str):
+        vector = self.model.encode(x)
+        self.index.add(id, vector)
 
-# def get_tags_id():
-#     label_dict = deepcopy(classifier.id_2_label)
-#     for id, label in id_2_label.item():
-#         label_dict[id + tag_offset] = label
-#     return label_dict
+    async def save_db(self):
+        self.index.save("db/main.index")
+    
+    def index_rank(self, query: str, topk: int):
+        query_embedding = self.model.encode(query)
+        result = self.index.search(query_embedding, topk)
+        return result.keys, result.distances
+    
+    def get_tags(self, query: str):
+        query_embedding = self.model.encode(query)
+        
+        if query_embedding.ndim == 1:
+            query_embedding = query_embedding[None, :]
+        result = self.classifier.predict(query_embedding)
+        department_label, department_label_id = self.classifier.get_label(result)
+        
+        result = self.topic_label_index.search(query_embedding, 5)
+        # print("\n".join(x[k] for k in result.keys))
+        # print(cluster_labels[result.keys])
+        most_similar = Counter(self.cluster_labels[result.keys]).most_common(1)[0][0]
+        # print(f"Most similar proposal: {most_similar}")
+        print([self.id_2_label[k] for k in self.cluster_labels[result.keys]])
+        
+        if self.id_2_label[most_similar] != None:
+            topic_label = self.id_2_label[most_similar].strip("\n")
+        else:
+            topic_label = None
+        
+        return department_label, topic_label
+
+ 
