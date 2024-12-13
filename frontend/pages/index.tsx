6e8e719d6e7f9a4b7e8ed95598d79f564eedf5a6
wytsai7660 <wytsai7660@gmail.com>
2024-12-13 14:18:09 +0800

    Frontend homepage
    - Remove the old `home.tsx`
    - Use `frontend/public/mockdata/proposals.json` to demonstrate the overall look (this should be removed afterward)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,25 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
+import { Container, SimpleGrid, Stack, Title } from '@mantine/core';
+import { Layout } from '@/components/Layout/Layout';
+import { ProposalCard } from '@/components/ProposalCard/ProposalCard';
+import data from '@/public/mockdata/proposals.json';
 
 export default function HomePage() {
+  const items = data.map((item) => <ProposalCard data={item} key={item.id} />);
+
   return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
+    <Layout>
+      <Container size="xl">
+        <Title order={2} mt="xl" mb="md">
+          熱門議題
+        </Title>
+        <SimpleGrid cols={{ base: 1, sm: 3 }}>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <Stack key={index}>
+              {items.slice((index * items.length) / 3, ((index + 1) * items.length) / 3)}
+            </Stack>
+          ))}
+        </SimpleGrid>
+      </Container>
+    </Layout>
   );
 }
