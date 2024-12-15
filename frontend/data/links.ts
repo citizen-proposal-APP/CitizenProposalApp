06c01e77780a33494d454f84e765eb77a70bb721
lululala0222 <jia930222@gmail.com>
2024-12-15 16:28:14 +0800

    update the data/link.ts with manual pages

--- frontend/data/links.ts
+++ frontend/data/links.ts
@@ -3,15 +3,15 @@ import { IconBook, IconBubbleText, IconBuildingBank, IconInfoCircle } from '@tab
 export const links = [
   { link: '/proposals', label: '議題與討論', icon: IconBubbleText },
   { link: '/departments', label: '認識部門', icon: IconBuildingBank },
-  { link: '/Manual/Manual', label: '使用說明', icon: IconBook },
+  { link: '/manual/manual', label: '使用說明', icon: IconBook },
   {
     link: '#2',
     label: '關於',
     icon: IconInfoCircle,
     links: [
-      { link: '/FAQ/FAQ', label: '常見問題' },
-      { link: '/ContactUs/ContactUs', label: '聯絡我們' },
-      { link: '/AboutUs/AboutUs', label: '關於我們' },
+      { link: '/faq/faq', label: '常見問題' },
+      { link: '/contact-us/contact-us', label: '聯絡我們' },
+      { link: '/about-us/about-us', label: '關於我們' },
     ],
   },
 ];
