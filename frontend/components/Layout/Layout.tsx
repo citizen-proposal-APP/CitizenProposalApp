8995f4af98edda6af0cf1accd9f9fdece23185fb
wytsai7660 <wytsai7660@gmail.com>
2024-12-18 07:54:43 +0800

    Support aside section in `Layout` component

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -3,14 +3,22 @@ import { AppShell } from '@mantine/core';
 import { HeaderMenu } from '@/components/HeaderMenu/HeaderMenu';
 import { NavbarNested } from '@/components/NavbarNested/NavbarNested';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+interface LayoutProps {
+  children: React.ReactNode;
+  aside?: React.ReactNode;
+}
+
+export function Layout({ children, aside }: LayoutProps) {
   const [opened, setOpened] = useState(false);
   const toggle = () => setOpened(!opened);
+
+  const [asideOpened, setAsideOpened] = useState(false);
 
   return (
     <AppShell
       header={{ height: 60 }}
       navbar={{ width: 300, breakpoint: 'sm', collapsed: { desktop: true, mobile: !opened } }}
+      aside={{ width: '25%', breakpoint: 'sm', collapsed: { desktop: true, mobile: !asideOpened } }}
       padding="md"
     >
       <AppShell.Header>
@@ -22,6 +30,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         </AppShell.Navbar>
       )}
       <AppShell.Main>{children}</AppShell.Main>
+      {aside && <AppShell.Aside p="md">{aside}</AppShell.Aside>}
     </AppShell>
   );
 }
